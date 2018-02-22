c50124955aa54f8b3a3f19bbfe8e9b80456c3b6d
rosscwagner <rosscwagner@gmail.com>
2018-02-21 20:06:54 -0500

    my firt command line commit!

---
+++ src/camera/Camera.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package camera;
+
+/**
+ *
+ * @author Ross Wagner
+ */
+public class Camera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
