50a839e99112d9e1182b3c55e15bfb96ff971fb2
rosscwagner <rosscwagner@gmail.com>
2018-03-02 13:17:00 -0500

    Rework so wall and car path are vertical

--- src/camera/camera.java
+++ src/camera/camera.java
@@ -1,37 +1,60 @@
-/*
+/**
+ * Author: Ross Wagner
+ * 
+ * Im interpreting the wall and the path of the car to be vertical to avoid 
+ * infinite slope.
+ * 
+ * To Do
+ * ------
+ * implement greedy alg for taking pictures.
+ *  
+ * 
  * 
  */
 package camera;
 
 import java.util.*;
 /**
- *
- * @author Ross Wagner
+ * This is the main class of the program. Its main takes input from stdin in the 
+ * form of <number of objects> <distance from axis of occluder> 
+ * <cordinates of objects (y,x)> y coordinate comes first because in the 
+ * specicifations for this problem the occluder and car path are horizontal however
+ * to avoid issues with infininite slope I decided to make them vertical.
+ * 
+ * 
  */
 public class camera {
 
+    /**
+     * obj represents objects that we are trying to take pictures of
+     * 
+     */
     public static class obj{
+        // location in index where this obj is stored
         int index;
-        float x,y;
-        float viewStart, viewEnd;
+        // cordinates
+        double x,y;
+        // place on y axis (car's path) where obj is visabl e 
+        double viewStart, viewEnd;
         boolean aboveWall;
         
-        public obj(int index,float x, float y, wall wall){
+        public obj(int index,double x, double y, wall wall){
             // init the obj
             this.index = index;
             this.x = x;
             this.y = y;
-            this.aboveWall = this.y > wall.height;
+            // 
+            this.aboveWall = this.x > wall.dist;
             if(aboveWall){
-                float slope1 = clacSlope(this.x, this.y, wall.openStart, wall.height);
-                float slope2 = clacSlope(this.x, this.y, wall.openEnd, wall.height);
-                // clac x intercepts
-                this.viewStart = -this.y/slope1 + this.x; // may cause issues if slope is inf
-                this.viewEnd = -this.y/slope2 + this.x;
+                double slope1 = clacSlope(this.x, this.y, wall.dist, wall.openStart);
+                double slope2 = clacSlope(this.x, this.y, wall.dist, wall.openEnd);
+                // clac y intercepts
+                this.viewStart = -this.x*slope1 + this.y; 
+                this.viewEnd = -this.x*slope2 + this.y;
             }
             else{
-                viewStart = Float.NEGATIVE_INFINITY;
-                viewEnd = Float.POSITIVE_INFINITY;
+                viewStart = Double.NEGATIVE_INFINITY;
+                viewEnd = Double.POSITIVE_INFINITY;
             }
         }
         
@@ -40,7 +63,7 @@ public class camera {
         //to string function for printing
         @Override
         public String toString(){
-            return "X: "+x+" Y: "+y+" Visable x= "+viewStart+" to "+viewEnd;
+            return "X: "+x+" Y: "+y+" Visable y = "+viewStart+" to "+viewEnd;
         }
         
     }
@@ -48,9 +71,9 @@ public class camera {
     public static class viewPoint implements Comparable<viewPoint>{
         obj obj;
         int value; // -1 for end +1 for start
-        float pos; // position on the x axis
+        double pos; // position on the x axis
         
-        public viewPoint(obj obj, int value, float pos){
+        public viewPoint(obj obj, int value, double pos){
             this.obj = obj;
             this.value = value;
             this.pos = pos;
@@ -58,16 +81,7 @@ public class camera {
         
         @Override
         public int compareTo(viewPoint other){
-            if(this.pos > other.pos){
-                return 1;
-            }
-            else if(this.pos < other.pos){
-                return -1;
-            } 
-            else{
-                // if tie -1 comes first
-                return  other.value - this.value;
-            }
+            return Double.compare(this.pos, other.pos);
         }
         
         @Override
@@ -78,10 +92,10 @@ public class camera {
     }
     
     public static class wall{
-        float height, openStart, openEnd;
+        double dist, openStart, openEnd;
         
-        public wall(float height, float openStart, float openEnd){
-            this.height = height;
+        public wall(double dist, double openStart, double openEnd){
+            this.dist = dist;
             this.openStart = openStart;
             this.openEnd = openEnd;
         }
@@ -95,11 +109,12 @@ public class camera {
         // get input from stdin
         Scanner in = new Scanner(System.in);
         int numObj = in.nextInt();
-        float wallDist = in.nextFloat();
-        float openingStart = in.nextFloat();
-        float openingEnd = in.nextFloat();
+        double wallDist = in.nextDouble();
+        double openingStart = in.nextDouble();
+        double openingEnd = in.nextDouble();
         
         wall wall = new wall(wallDist,openingStart, openingEnd);
+        System.out.println("Wall: "+wall.dist);
         
         obj[] objs = new obj[numObj];
         boolean[] used = new boolean[numObj];
@@ -111,10 +126,14 @@ public class camera {
         // load objs in into array
         for(int i = 0; i < numObj; i++){
             
-            obj tempObj = new obj(i,in.nextFloat(), in.nextFloat(), wall); 
+            double x,y;
+            y=in.nextDouble();
+            x=in.nextDouble();
+            
+            obj tempObj = new obj(i,x, y, wall); 
             objs[i] = tempObj;
             used[i] = false;
-            
+            System.out.println(tempObj);
             // get edges of viable area
             viewPoint tempVp1 = new viewPoint(tempObj, 1, tempObj.viewStart );
             viewPoint tempVp2 = new viewPoint(tempObj, -1, tempObj.viewEnd );
@@ -123,25 +142,31 @@ public class camera {
         }
         Collections.sort(vps);
         
+        //System.out.println(vps);
+        
+        
+        
+        
+        
+        
         
     
     
     
     }
     
-    public static float clacSlope(float x1, float y1, float x2, float y2){
-        if(x1 == x2){
-            if(y2 > y1){
-                return Float.POSITIVE_INFINITY;
-            }
-            else if(y2 < y1){
-                return Float.NEGATIVE_INFINITY;
-            }
-            else{
-                return Float.NaN;
-            }
-        }
-        return ((float)(y2-y1))/((float)(x2-x1));
+    /**
+     * Calculates the slope of two points given their x and y coordinates.
+     * 
+     * @param x1, 
+     * @param y1 
+     * @param x2 
+     * @param y2 
+     * 
+     * @return slope, the slope of the line with given points
+     */
+    public static double clacSlope(double x1, double y1, double x2, double y2){
+        return (y2-y1)/(x2-x1);
     }
     
 }
